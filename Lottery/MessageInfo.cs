b53d02443fa3a399fe6ca328992b7181be538055
Liu Zhongyu <lzy19980525@yeah.net>
2019-04-22 14:08:58 +0800

    添加基础类库：MessageInfo 与 MessageParser

---
+++ Lottery/MessageInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    /// <summary>
+    /// 包含每条记录解析出来的数据
+    /// </summary>
+    public class MessageInfo
+    {
+        /// <summary>
+        /// 消息发送者的 ID（可能是 QQ 号或者邮箱账号）
+        /// </summary>
+        public string ID { get; set; }
+        /// <summary>
+        /// 消息发送的日期时间
+        /// </summary>
+        public DateTime SentTime { get; set; }
+        /// <summary>
+        /// 包含参与抽奖信息的列表
+        /// </summary>
+        public ISet<string> KeySet { get; set; }
+        /// <summary>
+        /// 消息内容的 HashCode
+        /// </summary>
+        public int MessageHashCode { get; set; }
+    }
+}
---
+++ Lottery/MessageParser.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    /// <summary>
+    /// MessageParser - 从聊天记录中获取有效信息
+    /// </summary>
+    public class MessageParser
+    {
+        // 匹配消息头部的 Regex
+        private static readonly Regex HeaderRegex = new Regex(
+            @"^(?<dateTime>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}).*(\((?<id>\d*)\)|<(?<id>[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+)>)$",
+            RegexOptions.Compiled, TimeSpan.FromMilliseconds(200));
+        // 日期格式
+        private const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+        // 匹配中奖消息的 Regex
+        private static readonly Regex LottoryCodeRegex = new Regex(
+            @"#(?<code>[^\\#|.]+)#",
+            RegexOptions.Compiled, TimeSpan.FromMilliseconds(200));
+
+        private string firstLine;
+        private string secondLine;
+        private readonly StreamReader reader;
+
+        /// <summary>
+        /// 由给定文件创建 MessageParser 对象
+        /// </summary>
+        /// <param name="fileName">聊天记录的文件名</param>
+        public MessageParser(string fileName)
+        {
+            reader = new StreamReader(fileName, Encoding.Default);
+            firstLine = reader.ReadLine();
+            secondLine = reader.ReadLine();
+        }
+
+        /// <summary>
+        /// 判断是否还有下一条消息记录
+        /// </summary>
+        /// <returns>如果还有记录，则返回 true，否则返回 false</returns>
+        public bool HasNextMessage()
+        {
+            return !string.IsNullOrEmpty(firstLine);
+        }
+
+        /// <summary>
+        /// 解析并返回下一条消息
+        /// </summary>
+        /// <returns>下一条消息得到的 MessageInfo 对象</returns>
+        public MessageInfo NextMessage()
+        {
+            MessageInfo result = new MessageInfo();
+            if (IsHeader(firstLine))
+            {
+                ParseHeader(firstLine, out DateTime sentTime, out string id);
+                result.ID = id;
+                result.SentTime = sentTime;
+            }
+            ReadNext();
+            result.KeySet = new HashSet<string>();
+            StringBuilder builder = new StringBuilder();
+            while (HasNextMessage() && !IsHeader(firstLine))
+            {
+                Match m = LottoryCodeRegex.Match(firstLine);
+                while (m.Success)
+                {
+                    result.KeySet.Add(m.Result("${code}"));
+                    m = m.NextMatch();
+                }
+                builder.Append(firstLine);
+                ReadNext();
+            }
+            result.MessageHashCode = builder.ToString().GetHashCode();
+            return result;
+        }
+
+        private bool IsHeader(string str)
+        {
+            return HeaderRegex.Match(str).Success;
+        }
+
+        private void ParseHeader(string str, out DateTime sentTime, out string id)
+        {
+            Match m = HeaderRegex.Match(str);
+            if (m.Success)
+            {
+                sentTime = DateTime.ParseExact(m.Result("${dateTime}"), DateTimeFormat, CultureInfo.InvariantCulture);
+                id = m.Result("${id}");
+            }
+            else
+            {
+                // 注意，代码不应该运行到这里
+                sentTime = DateTime.Now;
+                id = string.Empty;
+            }
+        }
+
+        private void ReadNext()
+        {
+            firstLine = secondLine;
+            secondLine = reader.ReadLine();
+        }
+
+    }
+}
