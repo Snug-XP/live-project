f140f44aab31abc8638e6435d8668989f498f500
1055386758 <1055386758@qq.com>
2019-04-22 17:04:51 +0800

    更改User属性名和UserList

---
+++ Lottery/UserList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    class UserList
+    {
+        private List<User> userList = new List<User>();
+
+
+
+        public void Add(User user)
+        {
+            userList.Add(user);
+        }
+
+        public bool IsExist(string userID)
+        {
+            foreach(User p in userList)
+            {
+                if (userID.CompareTo(p.ID)!=0)
+                    return true;
+            }
+            return false;
+        }
+
+
+
+    }
