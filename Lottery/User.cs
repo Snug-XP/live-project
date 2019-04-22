ac2004fff8ef0d8d875d71345e2b0f54bac83730
huangquanhuan <46898209+huangquanhuan@users.noreply.github.com>
2019-04-22 18:17:40 +0800

    人员类最近发言的淘汰算法

--- Lottery/User.cs
+++ Lottery/User.cs
@@ -8,38 +8,95 @@ namespace Lottery
 {
     public class User
     {
-        private string id { get; set; }
-        private string name { get; set; }
+        public string ID { get; set; }
+        public string Name { get; set; }
         /// <summary>
         /// 身份
         /// </summary>
-        private string identity { get; set; }
+        public string Identity { get; set; }
         /// <summary>
         /// 平时发言次数
         /// </summary>
-        private int numberOfUsualSpeech { get; set; }
+        public int NumberOfUsualSpeech { get; set; }
         /// <summary>
         /// 抽奖发言次数
         /// </summary>
-        private int numberOfLotterySpeech { get; set; }
-        /// <summary>
-        /// 最近五次不同的发言的频次
-        /// </summary>
-        public List<Dictionary<string, int>> RecentSpeech { get; set; }
+        public int NumberOfLotterySpeech { get; set; }
         /// <summary>
         /// 活跃度
         /// </summary>
-        private int activity { get; set; }
+        public int Activity { get; set; }
+        /// <summary>
+        /// 最近五次不同的发言的频次
+        /// </summary>
+        public int[] Key = { -1,-1,-1,-1,-1 };
+        public int[] Value = { 0, 0, 0, 0, 0 };
+        private int index = 0;
 
-        public User(string id, string name, string identity, int numberOfUsualSpeech, int numberOfLotterySpeech,int activity)
+        public User(string id, string name, string identity, int numberOfUsualSpeech, int numberOfLotterySpeech, int activity)
         {
-            this.id = id;
-            this.name = name;
-            this.identity = identity;
-            this.numberOfUsualSpeech = numberOfUsualSpeech;
-            this.numberOfLotterySpeech = numberOfLotterySpeech;
-            this.activity = activity;
-         }
+            this.ID = id;
+            this.Name = name;
+            this.Identity = identity;
+            this.NumberOfUsualSpeech = numberOfUsualSpeech;
+            this.NumberOfLotterySpeech = numberOfLotterySpeech;
+            this.Activity = activity;
+        }
 
+        public int GetSumActivity()
+        {
+            return (2 * NumberOfUsualSpeech + NumberOfLotterySpeech + Activity);
+        }
+        /// <summary>
+        /// 淘汰算法
+        /// </summary>
+        /// <param name="hashcode"></param>
+        public void  weedOut (int hashcode)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                //键存在，值+1
+                if (hashcode == Key[index])
+                {
+                    Value[index]++;
+                    if (Value[index] / 2 == 10) Activity -= 50;
+                    else if (Value[index] > 20 && Value[index] <= 100)
+                    {
+                        Activity -= 3;
+                        Identity = "预警者";
+                    }
+                    else if (Value[index] > 100)
+                    {
+                        Activity -= 1008611;
+                        Identity = "危险者";
+                    }
+                    //活跃度<-1500000后不再减少（防止溢出）
+                    else if (Value[index] < -1500000) { };
+                    return;
+                }
+                else
+                {
+                    index++;
+                    if (index >= 5) index = 0;
+                }
+            }
+            //只比对四次-1，第五次直接替换
+            for (int i = 0; i < 4; i++)
+            {
+                if (-1 == Key[index])
+                {
+                    Key[index] = hashcode;
+                    Value[index] = 0;
+                    return;
+                }
+                else
+                {
+                    index++;
+                    if (index >= 5) index = 0;
+                }
+            }
+            Key[index] = hashcode;
+            Value[index] = 0;
+        }
     }
 }
